8715189104b55076e25aae9ff28cff3ac63309c2
Aman Parida <amantechz467@gmail.com>
2025-11-09 14:51:11 +0530

    Created Add Post controller

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,50 @@
+import { User } from "../models/user.model.js";
+import sharp from "sharp";
+import getDataUri from "../utils/dataURI";
+import cloudinary from "../utils/cloudinary.js";
+import { Post } from "../models/post.model.js";
+
+export const addNewPost = async (req, res) => {
+    try {
+        const { caption } = req.body;
+        const image = req.file;
+        const authorId = req.id;
+
+        if (!image) {
+            return res.status(400).json({
+                success: false,
+                message: "Image is required"
+            });
+        }
+
+        const optimizedImageBuffer = await sharp(image.buffer).resize({ width: 800, height: 800, fit: "inside" }).toFormat("jpeg", { quality: 80 }).toBuffer();
+        const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString("base64")}`;
+        const cloudinaryResponse = await cloudinary.uploader.upload(fileUri);
+
+        const post = await Post.create({
+            image: cloudinaryResponse.secure_url,
+            caption,
+            author: authorId
+        }, { new: true });
+
+        await User.findByIdAndUpdate(
+            authorId,
+            { $push: { posts: post._id } }
+        );
+
+        await post.populate({ path: "author", select: "-password" });
+
+        return res.status(201).json({
+            success: true,
+            message: "Post created successfully!",
+            post
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+
+        });
+    }
+}
+
--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     image: {
         type: String,
-        required: true
+        required: true 
     },
     caption: {
         type: String,
